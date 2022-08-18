84fc21aad329580daddf7729335aacd48535fe60
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-18 15:40:21 +0300

    Make API call in services.js

--- src/redux/services.js
+++ src/redux/services.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-unused-vars */
-/* eslint-disable import/prefer-default-export */
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3000';
@@ -7,6 +6,17 @@ const BASE_URL = 'http://localhost:3000';
 export const login = async (user) => {
   const response = await axios.post(`${BASE_URL}/users/sign_in`, {
     session: {
+      email: user.email,
+      password: user.password,
+    },
+  });
+  return response;
+};
+
+export const signup = async (user) => {
+  const response = await axios.post(`${BASE_URL}/users/`, {
+    user: {
+      name: user.name,
       email: user.email,
       password: user.password,
     },
