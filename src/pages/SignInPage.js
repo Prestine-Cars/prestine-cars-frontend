3b9bf0c3037ad513b276c289f272d69992375189
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-18 13:20:49 +0300

    Rename Login component

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Login from '../components/Login';
+import SignInForm from '../components/SignInForm';
 
 const SignInPage = () => (
   <>
     <section>
-      <Login />
+      <SignInForm />
     </section>
   </>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,19 +1,3 @@
-// import { applyMiddleware, combineReducers, createStore } from 'redux';
-// import thunk from 'redux-thunk';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-
-// import userReducer from './reducers/user';
-
-// const rootReducer = combineReducers({
-//   user: userReducer,
-// });
-
-// const store = createStore(
-//   rootReducer,
-//   composeWithDevTools(applyMiddleware(thunk)),
-// );
-
-// export default store;
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user';
 
