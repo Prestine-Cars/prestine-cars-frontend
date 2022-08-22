3c62cae701e50de22f1919f3447ad661d0552d17
selma <belhadjselma1@gmail.com>
2022-08-22 08:26:12 +0100

    Create the sidebar nav component & add it to the app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import CitiesPage from './pages/CitiesPage';
+import Sidebar from './components/Sidebar';
+import { authHeader } from './redux/services';
 
 const App = () => (
   <BrowserRouter>
-    <Routes>
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/cities" element={<CitiesPage />} />
-    </Routes>
+    <div className={authHeader ? 'sm:flex' : null}>
+      {authHeader ? <Sidebar /> : null}
+      <div className="flex-1">
+        <Routes>
+          <Route path="/signin" element={<SignInPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/cities" element={<CitiesPage />} />
+        </Routes>
+      </div>
+    </div>
   </BrowserRouter>
 );
 
