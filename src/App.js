7532f51b1e2213d70aa79a717c61e29fb4c9ae56
selma <belhadjselma1@gmail.com>
2022-08-22 11:50:46 +0100

    Add page for adding car

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import CitiesPage from './pages/CitiesPage';
-
+import AddCarPage from './pages/AddCarPage';
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/cities" element={<CitiesPage />} />
+      <Route path="/add_car" element={<AddCarPage />} />
     </Routes>
   </BrowserRouter>
 );
---
+++ src/pages/AddCarPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import CarForm from '../components/CarForm'
+const AddCarPage = () => {
+  return (
+    <div>
+      <CarForm />
+    </div>
+  )
+}
+
+export default AddCarPage
