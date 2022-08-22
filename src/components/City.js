633510a6d5e0226e1c6f5aa662200d6ee7fae79b
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-22 08:53:06 +0300

    Add logout button

--- src/components/City.js
+++ src/components/City.js
@@ -1,12 +1,23 @@
+// @ts-nocheck
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-const City = () => (
-  <>
-    <div>
-      <h1>I am a city</h1>
-    </div>
-    ;
-  </>
-);
+const City = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    dispatch(logout(navigate));
+  }
+  return (
+    <>
+      <div>
+        <h1>I am a city</h1>
+      </div>
+      <button type="button" onClick={handleLogout}>Logout</button>
+    </>
+  );
+};
 
 export default City;
