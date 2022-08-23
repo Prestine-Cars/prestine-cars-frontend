bc80dc84deb2574dbdb5a45652844658f2f9fc14
Munezero Christian <chamsf5@aol.com>
2022-08-23 10:20:26 +0200

    Add cities and city global state to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './reducers/user';
+import cityReducer from './reducers/city';
+import citiesReducer from './reducers/cities';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
+    cities: citiesReducer,
+    city: cityReducer,
   },
 });
 
