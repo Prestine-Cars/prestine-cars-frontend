70fcff86bb1446a51d3f6894f60e2572407c08d4
Munezero Christian <chamsf5@aol.com>
2022-08-25 00:20:19 +0200

    Add delete city request

---
+++ src/redux/reducers/deleteCity.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+const actions = {
+  DELETE_CITY_REQUEST: 'DELETE_CITY_REQUEST',
+  DELETE_CITY_RESPONSE: 'DELETE_CITY_RESPONSE',
+  DELETE_CITY_FAILURE: 'DELETE_CITY_FAILURE',
+};
+
+const initialState = {
+  loading: false,
+  message: '',
+  error: null,
+};
+
+export const deleteCity = (cityId, navigate) => (dispatch) => {
+  dispatch({ type: actions.DELETE_CITY_REQUEST });
+  axios
+    .delete(`http://localhost:3000/api/v1/cities/${cityId}`, {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+    })
+    .then((response) => {
+      dispatch({
+        type: actions.DELETE_CITY_RESPONSE,
+        payload: response.data,
+      });
+      navigate('/cities');
+    })
+    .catch((err) => {
+      dispatch({
+        type: actions.DELETE_CITY_FAILURE,
+        payload: err.response.data.error,
+      });
+    });
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actions.DELETE_CITY_REQUEST:
+      return {
+        loading: true,
+        message: '',
+        error: null,
+      };
+    case actions.DELETE_CITY_RESPONSE:
+      return {
+        loading: false,
+        message: action.payload,
+        error: null,
+      };
+    case actions.DELETE_CITY_FAILURE:
+      return {
+        loading: false,
+        message: '',
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
