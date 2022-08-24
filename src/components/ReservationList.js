ddcf82c9ad922244fe3a18d55d544eb430c56798
SirriRyisa <sirriryisa@gmail.com>
2022-08-24 19:23:27 +0100

    Add reservation list file

---
+++ src/components/ReservationList.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchReservations } from '../redux/actions/reservation';
+import ReservationCardItem from './ReservationCardItem';
+
+const ReservationList = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchReservations());
+  }, []);
+
+  const { reservations, loading } = useSelector((state) => state.reservation);
+  // console.log(reservations);
+  return (
+    <div className="main-container">
+      <div className="content-container">
+        <h1>Reserve a car with prestine-car</h1>
+        {loading === true && <p>Loading...</p> }
+        <ReservationCardItem reservations={reservations} />
+      </div>
+    </div>
+  );
+};
+
+export default ReservationList;
