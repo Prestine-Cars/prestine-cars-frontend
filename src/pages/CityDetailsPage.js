b92cb4a55f7a6450d02a3b6b6a794c081e9a7f04
SirriRyisa <sirriryisa@gmail.com>
2022-08-24 09:19:36 +0100

    Fix linters

--- src/pages/CityDetailsPage.js
+++ src/pages/CityDetailsPage.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React, { useEffect } from 'react';
 import { useNavigate, Link, useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
--- src/redux/services.js
+++ src/redux/services.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable */
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:3000';
