617d2ae421f754cc14184d77e6e388f7d4455677
Munezero Christian <chamsf5@aol.com>
2022-08-24 20:58:13 +0200

    add response to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,12 +4,14 @@ import cityReducer from './reducers/city';
 import citiesReducer from './reducers/cities';
 import carReducer from './reducers/car';
 import reservationReducer from './reducers/reservation';
+import addCityReducer from './reducers/addCity';
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     cities: citiesReducer,
     city: cityReducer,
+    add_city: addCityReducer,
     car: carReducer,
     reservation: reservationReducer,
   },
