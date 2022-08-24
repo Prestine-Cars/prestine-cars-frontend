ae71ad6c188715a9e9aa3fc63e7b5d62a284a49e
selma <belhadjselma1@gmail.com>
2022-08-24 19:32:10 +0100

    Update the deleteCar functioto get data from API

--- src/redux/services.js
+++ src/redux/services.js
@@ -49,19 +49,13 @@ export const createCar = async (car) => {
   return { ...response.data };
 };
 
-export const deleteCar = (cityId, CarId) => (dispatch) => {
-  API.deleteCar(cityId, CarId)
-    .then((response) => {
-      dispatch({
-        type: actionTypes.DELETE_CAR_SUCCESS,
-        payload: response.message,
-      });
-    }).catch((error) => {
-      dispatch({
-        type: actionTypes.DELETE_CAR_FAILURE,
-        payload: error.response.data.error,
-      });
-    });
+export const deleteCar = async (cityId, CarId) => {
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: authHeader(),
+  };
+  const response = await axios.delete(`${BASE_URL}/api/v1/cities/${cityId}/cars/${CarId}`, { headers });
+  return { ...response.data };
 };
 
 
