c04401f062897f521755b417482fa4d5ea242775
SirriRyisa <sirriryisa@gmail.com>
2022-08-24 02:13:17 +0100

    Add reservation link

---
+++ src/pages/CityDetailsPage.js
@@ -0,0 +1,69 @@
+import React, { useEffect } from 'react';
+import { useNavigate, Link, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCity } from '../redux/reducers/city';
+
+const CityDetailsPage = () => {
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const city = useSelector((state) => state.city);
+  const { loading, error, city: cityData } = city;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCity(id));
+  }, [id]);
+
+  const getCityId = (id, navigate) => {
+    navigate(`/cities/${id}/cars`);
+  };
+
+  return (
+    (loading && (
+      <div className="d-flex flex-column justify-content-center align-items-center vh-100">
+        <span className="spinner-border text-primary" />
+        <p>Loading...</p>
+      </div>
+    ))
+    || (error && (
+      <div className="alert alert-danger" role="alert">
+        {city.error}
+      </div>
+    )) || (
+      <div className="">
+        <div className="text-center">
+          <h2>
+            {cityData.name}
+            {' '}
+            Details
+          </h2>
+          <p>{cityData.description}</p>
+          <button
+            type="button"
+            className="btn btn-primary"
+            onClick={() => getCityId(id, navigate)}
+          >
+            Add car
+          </button>
+        </div>
+        <div className="">
+          {cityData.cars.map((car) => (
+            <div key={car.id} className="card">
+              <img src="..." className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{car.name}</h5>
+                <h5 className="card-title">{car.price}</h5>
+                <p className="card-text">{car.description}</p>
+                <Link to={`/cities/${id}/cars/${car.id}/add_resevation`} className="btn btn-primary">
+                  Reserve
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  );
+};
+
+export default CityDetailsPage;
