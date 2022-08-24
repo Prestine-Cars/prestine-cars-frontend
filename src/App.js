9860ed841efb66db3658d1917540ff5fcc7c51c0
Munezero Christian <chamsf5@aol.com>
2022-08-24 20:55:14 +0200

    Add a component for adding a new city

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
 import CitiesPage from './pages/CitiesPage';
+import AddCityPage from './pages/AddCityPage';
 import CityDetailsPage from './pages/CityDetailsPage';
 import AddCarPage from './pages/AddCarPage';
 import Sidebar from './components/Sidebar';
@@ -19,11 +20,14 @@ const App = () => (
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/" element={<CitiesPage />} />
           <Route path="/cities" element={<CitiesPage />} />
+          <Route path="/add_city" element={<AddCityPage />} />
           <Route path="/cities/:id" element={<CityDetailsPage />} />
           <Route path="/cities/:cityId/cars" element={<AddCarPage />} />
           <Route path="/reservations" element={<ReservationsPage />} />
-          <Route path="/cities/:cityId/cars/:carId/add_resevation" element={<AddReservationPage />} />
-
+          <Route
+            path="/cities/:cityId/cars/:carId/add_resevation"
+            element={<AddReservationPage />}
+          />
         </Routes>
       </div>
     </BrowserRouter>
