a485d2197ddf3e82d076a32627d58a3c19129251
SirriRyisa <sirriryisa@gmail.com>
2022-08-25 18:15:09 +0100

    Modify css for reservation cards

--- src/components/ReservationList.js
+++ src/components/ReservationList.js
@@ -1,8 +1,9 @@
-// @ts-nocheck
+/*eslint-disable*/
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservations } from '../redux/actions/reservation';
 import ReservationCardItem from './ReservationCardItem';
+import classes from './modules/CityDetail.module.css';
 
 const ReservationList = () => {
   const dispatch = useDispatch();
@@ -12,13 +13,15 @@ const ReservationList = () => {
 
   const { reservations, loading } = useSelector((state) => state.reservation);
   return (
-    <div className="main-container">
-      <div className="content-container">
-        <h1>Reserve a car with prestine-car</h1>
-        {loading === true && <p>Loading...</p> }
+    <section className={`${classes.content_container}`}>
+      {loading && <p>Loading...</p>}
+      <div className='text-center my-4 mw-70'>
+        <h2 className={`${classes.hero_text}`}>These are your Reservations</h2>
+      </div>
+      <div className={`pt-4 ${classes.car_wrapper}`}>
         <ReservationCardItem reservations={reservations} />
       </div>
-    </div>
+    </section>
   );
 };
 
