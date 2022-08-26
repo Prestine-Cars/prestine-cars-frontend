fe0292f82c5d1da2cf213b99b7ddd47757dfb289
Benmuiruri <ben.muiruri13@gmail.com>
2022-08-26 08:42:44 +0300

    Fix Sign up styling

--- src/components/SignInForm.js
+++ src/components/SignInForm.js
@@ -35,7 +35,7 @@ const SignInForm = () => {
         <img src={logoImg} className={classes.logo} alt="Logo" />
       </div>
       <div className="container pt-4 mt-4">
-        <div className="row d-flex justify-content-center align-items-center h-100">
+        <div className="row d-flex justify-content-center align-items-center">
           <div className="col-12 col-md-8 col-lg-6 col-xl-5">
             <div className={`card shadow-5-strong ${classes.sign_up_card}`}>
               <div className="card-body p-5 text-center">
